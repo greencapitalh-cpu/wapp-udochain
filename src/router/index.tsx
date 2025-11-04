bf137ae755e96972f663eaac3df9e9964cbeccdc
greencapitalh-cpu <greencapitalh@gmail.com>
2025-11-04 05:26:04 -0400

    Update index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,16 +1,11 @@
 // src/router/index.tsx
 import { createBrowserRouter } from "react-router-dom";
-import DashboardLayout from "../shared/DashboardLayout";
 import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <DashboardLayout />,
-    children: [
-      { index: true, element: <Home /> },
-      
-    ],
+    element: <Home />,
   },
 ]);
 
