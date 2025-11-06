2102ee2a77db54c4d7433dd1e7a2b8e0c9eaedc9
greencapitalh-cpu <greencapitalh@gmail.com>
2025-11-06 01:50:23 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// [04] vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: true, port: 5173 }
+  server: { port: 5173 },
+  define: { "process.env": {} }
 });
